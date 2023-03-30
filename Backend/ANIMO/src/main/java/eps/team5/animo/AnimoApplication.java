8835caab61d675b2a907cd8e7334bb3dd4a4294e
Sonia <soniagupta08@hotmail.com>
2023-03-31 00:06:58 +0100

    Initialise backend

---
+++ Backend/ANIMO/src/main/java/eps/team5/animo/AnimoApplication.java
@@ -0,0 +1,13 @@
+package eps.team5.animo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimoApplication.class, args);
+	}
+
+}
---
+++ Backend/ANIMO/src/test/java/eps/team5/animo/AnimoApplicationTests.java
@@ -0,0 +1,13 @@
+package eps.team5.animo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
